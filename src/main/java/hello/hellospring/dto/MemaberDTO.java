d1da89361975c5a020d3b29dedc09043cea3396a
GoWoong <popgun10@naver.com>
2022-03-27 23:10:34 +0900

    Lombok study

--- src/main/java/hello/hellospring/dto/MemaberDTO.java
+++ src/main/java/hello/hellospring/dto/MemaberDTO.java
@@ -1,41 +1,16 @@
 package hello.hellospring.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class MemaberDTO {
 
     private String name;
     private String email;
     private String organization;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    @Override
-    public String toString() {
-        return "MemaberDTO{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", organization='" + organization + '\'' +
-                '}';
-    }
 }
