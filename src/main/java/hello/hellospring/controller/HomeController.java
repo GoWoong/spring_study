69dff3ae688d4430570d01c376dd24625637c9d3
GoWoong <popgun10@naver.com>
2022-03-28 17:10:27 +0900

    use Logback

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,15 +1,31 @@
 package hello.hellospring.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){
-        return "home";
+    private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+    
+    @RequestMapping("/home")
+    public String hello(){
+        return "Hello World";
     }
+    
+    @PostMapping("log-test")
+    public void logTest(){
+        
+        LOGGER.trace("Trace Log");
+        LOGGER.debug("Debug Log");
+        LOGGER.info("Info Log");
+        LOGGER.warn("Warn Log");
+        LOGGER.error("Error Log");
+    }
+
 }
 
 
